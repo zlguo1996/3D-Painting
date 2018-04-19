bc0ee12ab188cd399ee660df29d2e7d5605a5322
neconeconeco <1270775934@qq.com>
2018-04-19 10:43:21 +0800

    Initialization

---
+++ Assets/Scripts/Test/Bluetooth.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using UnityEngine;
+
+public class Bluetooth : MonoBehaviour {
+
+	private SerialPort sp = new SerialPort("/dev/cu.PressPen-DevB", 9600);
+	private bool mylight = true;
+
+	// Use this for initialization
+	void Start () {
+		sp.Open();
+		sp.ReadTimeout = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (sp.IsOpen) {
+			int pres = sp.ReadByte ();
+			Vector3 position = this.transform.position;
+			this.GetComponent<Transform> ().position = new Vector3 (position.x, pres / 100.0f, position.z);
+		}
+	}
+
+	void OpenLight(){
+		if (sp.IsOpen) {
+			try {
+				sp.Write("1");
+			}
+			catch(System.Exception) {
+			}
+		}
+	}
+
+	void CloseLight(){
+		if (sp.IsOpen) {
+			try {
+				sp.Write("2");
+			}
+			catch(System.Exception) {
+			}
+		}
+	}
+
+	public void changeLight(){
+		if(mylight) OpenLight();
+		else CloseLight();
+		mylight = !mylight;
+	}
+}
---
+++ Assets/Scripts/Test/Test.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class Test : MonoBehaviour {
+
+	public int size=10;
+
+	[DllImport("Wrapper")]
+	private static extern int [,] fillArray(int size);
+
+	[DllImport("Wrapper")]
+	private static extern int add (int a, int b);
+
+//	[DllImport("Wrapper", CharSet=CharSet.Auto,
+//		CallingConvention=CallingConvention.StdCall)]
+//	private static extern int concat (string a, string b);
+
+	[DllImport("Wrapper", CallingConvention = CallingConvention.Cdecl)]
+	private static extern byte[] rstr ();
+
+	[DllImport("Wrapper")]
+	private static extern int s_in (StringBuilder ch);
+
+	// Use this for initialization
+	void Start () {
+		int [,] array;
+		array = fillArray(size);
+
+		int a = 0;
+		int b = add (a, a);
+
+		StringBuilder sb = new StringBuilder ("abc", 30);
+		int c = s_in (sb);
+
+		byte[] bt = rstr();
+		Debug.Log (bt[0]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
